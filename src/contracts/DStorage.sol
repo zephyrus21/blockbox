490a7b8cb5fe1ba5bb308727273e5bcd227d34ec
zephyrus21 <pandey.piyus@gmail.com>
2022-04-28 16:18:31 +0530

    added ipfs funcs

--- src/contracts/DStorage.sol
+++ src/contracts/DStorage.sol
@@ -3,33 +3,71 @@ pragma solidity ^0.8.13;
 
 contract DStorage {
     string public name = "DStorage";
+    uint256 public fileCount = 0;
+    mapping(uint256 => File) public files;
 
-    // Number of files
-    // Mapping fileId=>Struct
+    struct File {
+        uint256 fileId;
+        string fileHash;
+        uint256 fileSize;
+        string fileType;
+        string fileName;
+        string fileDescription;
+        uint256 uploadTime;
+        address payable uploader;
+    }
 
-    // Struct
-
-    // Event
+    event FileUploaded(
+        uint256 fileId,
+        string fileHash,
+        uint256 fileSize,
+        string fileType,
+        string fileName,
+        string fileDescription,
+        uint256 uploadTime,
+        address payable uploader
+    );
 
     constructor() public {}
 
-    // Upload File function
+    function uploadFile(
+        string memory _fileHash,
+        uint256 _fileSize,
+        string memory _fileType,
+        string memory _fileName,
+        string memory _fileDescription
+    ) public {
+        require(bytes(_fileHash).length > 0);
+        require(bytes(_fileType).length > 0);
+        require(bytes(_fileDescription).length > 0);
+        require(bytes(_fileName).length > 0);
+        require(msg.sender != address(0));
+        require(_fileSize > 0);
 
-    // Make sure the file hash exists
+        // Increment file id
+        fileCount++;
 
-    // Make sure file type exists
-
-    // Make sure file description exists
-
-    // Make sure file fileName exists
-
-    // Make sure uploader address exists
-
-    // Make sure file size is more than 0
-
-    // Increment file id
-
-    // Add File to the contract
-
-    // Trigger an event
+        // Add File to the contract
+        files[fileCount] = File(
+            fileCount,
+            _fileHash,
+            _fileSize,
+            _fileType,
+            _fileName,
+            _fileDescription,
+            block.timestamp,
+            payable(msg.sender)
+        );
+        // Trigger an event
+        emit FileUploaded(
+            fileCount,
+            _fileHash,
+            _fileSize,
+            _fileType,
+            _fileName,
+            _fileDescription,
+            block.timestamp,
+            payable(msg.sender)
+        );
+    }
 }
